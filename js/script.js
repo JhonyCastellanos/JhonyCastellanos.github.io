73ac1694ebeaf1fc389b3f980c3e966af3e31069
jhonycastellanos <jhonycastellanos12@gmail.com>
2023-08-14 16:07:39 -0500

    Update info

--- js/script.js
+++ js/script.js
@@ -37,21 +37,6 @@ window.onscroll = () => {
   navbar.classList.remove("active");
 };
 
-//Scroll reaveal
-ScrollReveal({
-  reset: true,
-  distance: "80px",
-  duration: 2000,
-  delay: 200,
-});
-
-ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
-ScrollReveal().reveal(".home-img, .education-container, .portfolio-box", {
-  origin: "bottom",
-});
-ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
-ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
-
 //Typed js
 const typed = new Typed(".multiple-text", {
   strings: ["Programador", "Desarrollador de software", "Full-Stack"],
